7d857f10d65877dc60abb0c2215da278e44dd916
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-07-17 10:02:18 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function (req, res) {
+  Article.find(function (err, results) {
+    if (!err) {
+      res.send(results);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post("/articles", function (req, res) {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  });
+
+  newArticle.save(function (err) {
+    if (!err) {
+      res.send("Successfully added new article.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started at port 3000");
+});
